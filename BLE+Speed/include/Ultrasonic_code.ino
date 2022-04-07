683114484289786bf4cf1a46799d1d0903cc0c9d
SunishaMF <sunisha.fernandez@berkeley.edu>
2022-04-07 14:41:17 -0700

    IR sensor and Ultrasonic sensor files

---
+++ BLE+Speed/include/Ultrasonic_code.ino
@@ -0,0 +1,32 @@
+#define echoPin 25 // attach pin D2 Arduino to pin Echo of HC-SR04
+#define trigPin 27 //attach pin D3 Arduino to pin Trig of HC-SR04
+
+// defines variables
+long duration; // variable for the duration of sound wave travel
+int distance; // variable for the distance measurement
+
+void setup() {
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
+  pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
+  Serial.begin(115200); // // Serial Communication is starting with 9600 of baudrate speed
+
+}
+void loop() {
+  // Clears the trigPin condition
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+  // Displays the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+  delay(500);
+
+}
